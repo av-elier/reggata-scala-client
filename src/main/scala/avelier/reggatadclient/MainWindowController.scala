7af995b42c38cfe1ebe88f20aa0780a16228e9a3
av-elier <adelier1111@gmail.com>
2016-10-24 20:13:41 +0300

    move files crawling out of javafx platform thread

--- src/main/scala/avelier/reggatadclient/MainWindowController.scala
+++ src/main/scala/avelier/reggatadclient/MainWindowController.scala
@@ -3,9 +3,11 @@ package avelier.reggatadclient
 import java.io.{File, IOException}
 import java.net.URL
 import java.util.ResourceBundle
+import java.util.concurrent.{ScheduledExecutorService, ScheduledThreadPoolExecutor}
 import javafx.fxml.{FXML, Initializable}
 import javafx.scene.control.{TreeTableColumn, TreeTableView}
 
+import scala.concurrent.{ExecutionContext, Future}
 import scalafx.Includes._
 import scalafx.application.Platform
 import scalafx.beans.property.ReadOnlyStringWrapper
@@ -18,29 +20,31 @@ class MainWindowController extends Initializable {
   @FXML var filesTree: TreeTableView[File] = _
   @FXML var dirsColName: TreeTableColumn[File, String] = _
 
+  implicit val executor: ExecutionContext = ExecutionContext.fromExecutor(new ScheduledThreadPoolExecutor(1))
+
   override def initialize(url: URL, rb: ResourceBundle) {
     val currentDir = new File("/home/av-elier/tmp")
     val selectedDir = new File("/home/av-elier/tmp/.reggata")
     dirsColName.cellValueFactory = x => new ReadOnlyStringWrapper(dirsColName, "file", x.value.getValue.getName)
-    Platform.runLater(findFiles(currentDir, selectedDir))
+    Future(findFiles(currentDir, selectedDir))
   }
 
   private def findFiles(dir: File, expandTo: File, parent: TreeItem[File] = null) {
     val root = new TreeItem[File](dir)
-    root.setExpanded(expandTo.getAbsolutePath startsWith dir.getAbsolutePath)
+    Future(Platform.runLater(root.setExpanded(expandTo.getAbsolutePath startsWith dir.getAbsolutePath)))
     try {
       val files = dir.listFiles()
       for (file <- files) {
         if (file.isDirectory) {
           findFiles(file, expandTo, root)
         } else {
-          root.getChildren.add(new TreeItem(file))
+          Future(Platform.runLater(root.getChildren.add(new TreeItem(file))))
         }
       }
       if (parent == null) {
-        filesTree.setRoot(root)
+        Future(Platform.runLater(filesTree.setRoot(root)))
       } else {
-        parent.getChildren.add(root)
+        Future(Platform.runLater(parent.getChildren.add(root)))
       }
     } catch {
       case e: IOException => e.printStackTrace()
